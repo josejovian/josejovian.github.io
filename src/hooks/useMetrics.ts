2c0676d3b936d8a5ff5bb737d74153621da55536
Jose Jovian <josejovian55@gmail.com>
2022-12-15 23:44:12 +0700

    chore: tweak useMetrics

--- src/hooks/useMetrics.ts
+++ src/hooks/useMetrics.ts
@@ -1,12 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState, useCallback } from "react";
 import { ScrollContext, WidthContext } from "@/src/contexts";
 
 export function useMetrics() {
 	const { width } = useContext(WidthContext);
 	const { scroll } = useContext(ScrollContext);
+	const [initWidth, setInitWidth] = useState(width);
+
+	const handleGetInitialWidth = useCallback(() => {
+		setInitWidth(document.documentElement.offsetWidth);
+	}, []);
+
+	useEffect(() => {
+		handleGetInitialWidth();
+	}, [handleGetInitialWidth]);
 
 	return {
-		width,
+		width: width === 0 ? initWidth : width,
 		scroll,
 	};
 }
